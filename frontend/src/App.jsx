309494502f0e1da4e77812e756178a08d26a518f
Abhijith cb <abhijithcb706@gmail.com>
2024-05-27 21:13:20 +0530

    navbar added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
-    <h1 className="text-3xl text-emerald-400 text-center bg-black">
-    Hello world!
-  </h1>
+    <>
+    <Navbar/>
+    </>
   )
 }
 
