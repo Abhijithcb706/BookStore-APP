7d97a527c71a1746bf9be5365a7dc16dbeab78dd
Abhijith cb <abhijithcb706@gmail.com>
2024-05-30 00:43:48 +0530

    connected fronted & backend with Axios

--- frontend/src/components/Course.jsx
+++ frontend/src/components/Course.jsx
@@ -1,9 +1,23 @@
-import React from "react";
-import Cards from './Cards'
-import lists from '../../public/list.json'
+import React, { useEffect, useState } from "react";
+import Cards from "./Cards";
+import axios from "axios";
+// import lists from "../../public/list.json";
 import { Link } from "react-router-dom";
 
 function Course() {
+  const [book, setBook] = useState([]);
+  useEffect(() => {
+    const getBook = async () => {
+      try {
+       const res= await axios.get("http://localhost:8000/book");
+       console.log(res.data);
+       setBook(res.data)
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getBook();
+  },[]);
   return (
     <>
       <div className="container max-w-screen-2xl mx-auto md:px-20 px-4">
@@ -16,24 +30,21 @@ function Course() {
             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis
             ipsa rerum veritatis laborum quas officia perspiciatis consequuntur
             culpa dolorem tempora eius dolore magni et, iusto, saepe enim
-            suscipit, expedita illum.Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis
-            ipsa rerum veritatis laborum quas officia perspiciatis consequuntur
-            culpa dolorem tempora eius dolore magni et, iusto, saepe enim
-            suscipit, expedita illum.
+            suscipit, expedita illum.Lorem ipsum dolor sit, amet consectetur
+            adipisicing elit. Debitis ipsa rerum veritatis laborum quas officia
+            perspiciatis consequuntur culpa dolorem tempora eius dolore magni
+            et, iusto, saepe enim suscipit, expedita illum.
           </p>
-          <Link to='/'>
-          <button className="mt-6 bg-pink-500 text-white px-4 py-2 rounded-md hover:bg-pink-700 duration-300">
+          <Link to="/">
+            <button className="mt-6 bg-pink-500 text-white px-4 py-2 rounded-md hover:bg-pink-700 duration-300">
               Back
             </button>
           </Link>
-          
         </div>
         <div className="mt-12 grid grid-cols-1 md:grid-cols-4">
-            {
-                lists.map((item)=>(
-                    <Cards key={item.id} item={item}/>
-                ))
-            }
+          {book.map((item) => (
+            <Cards key={item.id} item={item} />
+          ))}
         </div>
       </div>
     </>
--- frontend/src/components/Freebook.jsx
+++ frontend/src/components/Freebook.jsx
@@ -1,15 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
 
-import lists from "../../public/list.json";
-import Cards from './Cards'
+import axios from "axios";
 
+import Cards from "./Cards";
 function Freebook() {
-  const filterData = lists.filter((data) => data.category === "free");
-  console.log(filterData);
+  const [book, setBook] = useState([]);
+  useEffect(() => {
+    const getBook = async () => {
+      try {
+        const res = await axios.get("http://localhost:8000/book");
+
+        const data = res.data.filter((data) => data.category === "free");
+        console.log(data);
+        setBook(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getBook();
+  }, []);
 
   var settings = {
     dots: true,
@@ -25,47 +38,47 @@ function Freebook() {
           slidesToShow: 3,
           slidesToScroll: 3,
           infinite: true,
-          dots: true
-        }
+          dots: true,
+        },
       },
       {
         breakpoint: 600,
         settings: {
           slidesToShow: 2,
           slidesToScroll: 2,
-          initialSlide: 2
-        }
+          initialSlide: 2,
+        },
       },
       {
         breakpoint: 480,
         settings: {
           slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-    ]
+          slidesToScroll: 1,
+        },
+      },
+    ],
   };
-
   return (
     <>
-      <div className="container max-w-screen-2xl mx-auto md:px-20 px-4 ">
-       <div>
-       <h1 className="font-semibold text-xl pb-2">Free offered Courses</h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio dolore
-          nobis aperiam, illo aut eveniet ratione explicabo obcaecati sapiente.
-        </p>
-       </div>
+      <div className=" max-w-screen-2xl container mx-auto md:px-20 px-4">
         <div>
-        <Slider {...settings}>
-        {filterData.map((item)=>(
-            <Cards item={item} key={item.id}/>
-        ))}
-      </Slider>
+          <h1 className="font-semibold text-xl pb-2">Free Offered Courses</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Accusantium veritatis alias pariatur ad dolor repudiandae eligendi
+            corporis nulla non suscipit, iure neque earum?
+          </p>
+        </div>
+
+        <div>
+          <Slider {...settings}>
+            {book.map((item) => (
+              <Cards item={item} key={item.id} />
+            ))}
+          </Slider>
         </div>
       </div>
     </>
   );
 }
-
 export default Freebook;
